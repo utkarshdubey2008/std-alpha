c1b2ac1d9309b952717e2a2ac104a970e4b440bb
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2025-05-17 10:15:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,4 +20,4 @@ app.get("/download", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); // <- fixed closing parenthesis
